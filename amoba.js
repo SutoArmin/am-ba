84a1a955e480e30c25b1d2b7913df6b55f2009a3
Sütő Ármin <suto.armin@mechwart.local>
2024-01-29 12:02:13 +0100

    js

---
+++ amoba.js
@@ -0,0 +1,12 @@
+let jatekos = "x"
+
+function kattintas(gomb){
+    gomb.innerText = jatekos
+    gomb.disabled = true
+    if(jatekos == "x"){
+        jatekos = "o"
+    }else{
+        jatekos = "x"
+    }
+
+}
